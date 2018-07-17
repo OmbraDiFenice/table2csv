aeeb627e01221aa69e70ec4416e74414c54b150b
Jose Erick Carreon <joserick.92@gmail.com>
2018-07-17 11:11:30 -0500

    Change MIME type to csv (#2)
    Change the MIME to "text/csv"  for open directly the application appropriate to the file .csv

--- src/table2csv.js
+++ src/table2csv.js
@@ -25,7 +25,7 @@
 	// taken from http://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
 	function download(filename, text) {
 		var element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+		element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(text));
 		element.setAttribute('download', filename);
 		
 		element.style.display = 'none';
